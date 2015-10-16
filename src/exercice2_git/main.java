3705aa19510853768a7c076bd60e42820bd8d42d
JordanDraux <jordan.draux@hotmail.com>
2015-10-16 16:37:45 +0200

    Modif par Jordan D

--- src/exercice2_git/main.java
+++ src/exercice2_git/main.java
@@ -1,15 +1,19 @@
 package exercice2_git;
 
-public class main {
+public class main 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
 	}
 	
 	public void factorielle (int n)
 	{
+		int x =0;
 		
+		x++;
 	}
 
 }
