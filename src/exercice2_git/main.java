878275a39528063a3220de9b9a7fc815088e9616
Haveaux <valentin.haveaux@gmail.com>
2015-10-16 16:03:50 +0200

    premiere version

---
+++ src/exercice2_git/main.java
@@ -0,0 +1,15 @@
+package exercice2_git;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void factorielle (int n)
+	{
+		
+	}
+
+}
